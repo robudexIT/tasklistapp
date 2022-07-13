2be20e1a3f0d8a98a28d3b1b499fa71ff8e3aa9e
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2022-07-14 02:30:34 +0800

    Adding frontend and modify README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # taskListApp
-This Project is my own version of TodoList Using:
+This Project is my own version of TodoList 
+
+
+#For Master Branch (functional but very simple app with no authentication, styling, api endpoints and its running locally on my computer)
 NodeJS/Express - Backend/API
 VueJs - FrontEnd
+DATABASE: MYSQL
+
+
+
---
+++ frontend/tasklist/src/App.vue
@@ -0,0 +1,88 @@
+<script >
+   export default {
+    data(){
+        return {
+            taskList: {},
+            name: 'Rogmer Bulaclac',
+            taskName: ''
+        }
+    },
+    methods: {
+        async getTaskList(){
+            const taskList = await fetch('http://210.1.86.214:3000/getTaskList',{
+                method:'GET',
+              
+            })
+            this.taskList = await taskList.json()
+            console.log(this.taskList)
+        },
+        async addTask(){
+            const data = { taskName: this.taskName}
+            const addTask = await fetch('http://210.1.86.214:3000/task',{
+                headers: {
+                    'Content-Type': 'application/json'
+                    
+                },
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+             const response = await addTask.json()
+             if(response.taskId){
+                alert(response.message)
+                this.getTaskList()
+                this.taskName = ''
+             }
+             
+
+        },
+        async deleteTask(e){
+            console.log(e.target.id)
+            const taskId = e.target.id
+            try {
+               const deleteTask = await fetch(`http://210.1.86.214:3000/task/${taskId}`,{
+                    method: 'DELETE'
+                })
+                const response = await deleteTask.json()
+                 if(response.deleteTask){
+                    alert(response.message)
+                    this.getTaskList()
+                 }
+            }catch(e){
+                console.log(e)
+            }
+        },
+        test(){
+            alert('test')
+        }
+    },
+    computed: {
+        allTask(){
+            return this.taskList
+        }
+    },
+    created(){
+        this.getTaskList()
+    }
+   }
+</script>
+
+<template>
+ <h1>Welcome to TaskList App</h1>
+ <ul>
+    <li v-for="task in taskList.taskList" :key="task.taskId">
+        <p>{{task.taskName}} <span> <button v-bind:id="task.taskId" @click="deleteTask">Delete</button></span></p>
+       
+        <!-- <button>Update</button> -->
+    </li>
+ </ul>
+ <!-- <button @click="getTaskList">GETTASKLIST</button> -->
+ <form >
+  
+    <input type="text"  v-model="taskName">
+    <button @click.prevent="addTask">AddTask</button>
+ </form>
+</template>
+
+<style>
+
+</style>
